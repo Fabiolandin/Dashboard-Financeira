b723433ad15db040c867c6df9fdcae393fad6620
Fabio Landin <flandin1990@gmail.com>
2026-03-11 09:58:40 -0300

    feat adicionando a pagina inicial

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,33 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from '@/components/ui/card'
+import { Input } from '@/components/ui/input';
+import { Link } from 'react-router';
+
 const SignupPage = () => {
-    return(
-        <h1>SIgnup</h1>
+    return (
+        <div className='flex flex-col h-screen w-screen items-center justify-center gap-3'>
+            <Card className="w-[500px]">
+                <CardHeader>
+                    <CardTitle>Crie a sua conta</CardTitle>
+                    <CardDescription>Insira os dados abaixo.</CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                    <Input placeholder="Digite seu nome"/>
+                    <Input placeholder="Digite seu sobrenome"/>
+                    <Input placeholder="Digite seu e-mail"/>
+                    <Input type="password" placeholder="Digite sua senha"/>
+                </CardContent>
+                <CardFooter>
+                    <Button className="w-full">Criar conta</Button>
+                </CardFooter>
+            </Card>
+            <div className='flex items-center justify-center'>
+                <p className='text-center opacity-50'>Já possui uma conta?</p>
+                <Button variant="link" asChild>
+                    <Link to="/login">Faça login </Link>
+                    </Button>
+            </div>
+        </div>
     )
 }
 

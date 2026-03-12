869907f96a0d0e0ca475fb5a84e5ddae62d4a317
Fabio Landin <flandin1990@gmail.com>
2026-03-12 09:51:46 -0300

    fix finalizando pagina de login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,31 +1,90 @@
 import PasswordInput from '@/components/password-inputs';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from '@/components/ui/card'
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
+import { z } from 'zod'
+
+const loginSchema = z.object({
+    email: z.string().email({
+        message: 'O e-mail é inválido.',
+    })
+        .trim().min(1, {
+            message: 'O e-mail é obrigatório.',
+        }),
+
+    password: z.string().trim().min(6, {
+        message: 'A senha deve ter no mínimo 6 caracteres.'
+    })
+})
 
 const LoginPage = () => {
+    const methods = useForm({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        }
+    })
+
+    const handleSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
         <div className='flex flex-col h-screen w-screen items-center justify-center gap-3'>
-            <Card className="w-[500px]">
-                <CardHeader>
-                    <CardTitle className="items-center">Entre na sua conta</CardTitle>
-                    <CardDescription>Insira seus dados abaixo.</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <Input placeholder="Digite sua senha" />
-                    <PasswordInput />
-                </CardContent>
-                <CardFooter>
-                    <Button className="w-full">Fazer Login</Button>
-                </CardFooter>
-            </Card>
-            <div className='flex items-center justify-center'>
-                <p className='text-center opacity-50'>Ainda não possui uma conta?</p>
-                <Button variant="link" asChild>
-                    <Link to="/signup">Crie agora </Link>
-                </Button>
-            </div>
+            <Form {...methods}>
+                <form onSubmit={methods.handleSubmit(handleSubmit)}>
+                    <Card className="w-[500px]">
+                        <CardHeader>
+                            <CardTitle className="items-center">Entre na sua conta</CardTitle>
+                            <CardDescription>Insira seus dados abaixo.</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            {/* Input de email */}
+                            <FormField
+                                control={methods.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Digite seu email" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            {/* Input de senha */}
+                            <FormField
+                                control={methods.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Senha</FormLabel>
+                                        <FormControl>
+                                            <PasswordInput {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </CardContent>
+                        <CardFooter>
+                            <Button className="w-full">Fazer Login</Button>
+                        </CardFooter>
+                    </Card>
+                    <div className='flex items-center justify-center'>
+                        <p className='text-center opacity-50'>Ainda não possui uma conta?</p>
+                        <Button variant="link" asChild>
+                            <Link to="/signup">Crie agora </Link>
+                        </Button>
+                    </div>
+                </form>
+            </Form>
         </div>
     )
 }

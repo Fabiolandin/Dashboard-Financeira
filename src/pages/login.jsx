6c340824a82bcb2ba39fd864c4311a1cc8f1fbcc
Fabio Landin <flandin1990@gmail.com>
2026-03-12 09:18:59 -0300

    add: card de login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,32 @@
+import PasswordInput from '@/components/password-inputs';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from '@/components/ui/card'
+import { Input } from '@/components/ui/input';
+import { Link } from 'react-router';
+
 const LoginPage = () => {
     return (
-        <h1> Login Page</h1>
+        <div className='flex flex-col h-screen w-screen items-center justify-center gap-3'>
+            <Card className="w-[500px]">
+                <CardHeader>
+                    <CardTitle className="items-center">Entre na sua conta</CardTitle>
+                    <CardDescription>Insira seus dados abaixo.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Input placeholder="Digite sua senha" />
+                    <PasswordInput />
+                </CardContent>
+                <CardFooter>
+                    <Button className="w-full">Fazer Login</Button>
+                </CardFooter>
+            </Card>
+            <div className='flex items-center justify-center'>
+                <p className='text-center opacity-50'>Ainda não possui uma conta?</p>
+                <Button variant="link" asChild>
+                    <Link to="/signup">Crie agora </Link>
+                </Button>
+            </div>
+        </div>
     )
 }
 

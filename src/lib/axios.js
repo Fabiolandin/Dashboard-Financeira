6fe33eb2d834a67bdd781ec09ed412890ea28019
Fabio Landin <flandin1990@gmail.com>
2026-03-17 09:32:49 -0300

    feat:garantindo que o access token é atualizado quando dar o erro 401

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from '@/constants/local-storage'
+import { LOCAL_STORAGE_ACCESS_TOKEN_KEY, LOCAL_STORAGE_REFRESH_TOKEN_KEY } from '@/constants/local-storage'
 
 export const protectedApi = axios.create({
     baseURL: "https://fullstackclub-finance-dashboard-api.onrender.com",
@@ -15,6 +15,39 @@ protectedApi.interceptors.request.use((request) => {
     if (!accessToken) {
         return request
     }
-    request.headers.Authorizaxtion = `Bearer ${accessToken}`
+    request.headers.Authorization = `Bearer ${accessToken}`
     return request
 })
+
+protectedApi.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const request = error.config
+        const refreshToken = localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY)
+        if (!refreshToken) {
+            return Promise.reject(error)
+        }
+
+        if (error.response.status == 401 &&
+             !request._retry &&
+              !request.url.includes('/auth/refresh')
+            ) {
+            request._retry = true
+            try {
+                const response = await protectedApi.post('/auth/refresh', {
+                    refreshToken,
+                })
+                const newAccesToken = response.data.accessToken
+                const newRefreshToken = response.data.refreshToken
+                localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, newAccesToken)
+                localStorage.setItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY, newRefreshToken)
+                request.headers.Authorization = `Bearer ${newAccesToken}`
+                return protectedApi(request)
+            } catch (refreshError) {
+                localStorage.removeItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
+                localStorage.removeItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY)
+                console.error(refreshError)
+            }
+        }
+        return Promise.reject(error)
+    })

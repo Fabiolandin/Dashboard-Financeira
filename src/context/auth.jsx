5003d8e90ba06a8710aa3ccd9f4ca25c6533bc57
Fabio Landin <flandin1990@gmail.com>
2026-03-17 08:56:55 -0300

    feat: adicionando protect e public api

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -1,5 +1,5 @@
 import { LOCAL_STORAGE_ACCESS_TOKEN_KEY, LOCAL_STORAGE_REFRESH_TOKEN_KEY } from '@/constants/local-storage';
-import { api } from '@/lib/axios';
+import { protectedApi, publicApi } from '@/lib/axios';
 import { useMutation } from '@tanstack/react-query'
 import { createContext, useContext, useEffect, useState } from 'react'
 import { toast } from 'sonner';
@@ -32,7 +32,7 @@ export const AuthContextProvider = ({ children }) => {
     const signupMutation = useMutation({
         mutationKey: ['signup'],
         mutationFn: async (variables) => {
-            const response = await api.post('/users', {
+            const response = await publicApi.post('/users', {
                 first_name: variables.firstName,
                 last_name: variables.lastName,
                 email: variables.email,
@@ -45,7 +45,7 @@ export const AuthContextProvider = ({ children }) => {
     const loginMutation = useMutation({
         mutationKey: ['login'],
         mutationFn: async (variables) => {
-            const response = await api.post('/users/login', {
+            const response = await publicApi.post('/users/login', {
                 password: variables.password,
             })
             return response.data
@@ -59,7 +59,7 @@ export const AuthContextProvider = ({ children }) => {
                 const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
                 const refreshToken = localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY)
                 if (!accessToken && !refreshToken) return
-                const response = await api.get('/users/me')
+                const response = await protectedApi.get('/users/me')
                 setUser(response.data)
             } catch (error) {
                 setUser(null)

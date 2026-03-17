642c34c70be03f0c682e7220c8cf1a32fcca652f
Fabio Landin <flandin1990@gmail.com>
2026-03-17 10:58:08 -0300

    refactor: garantindo que as propriedades do usuario são retornadas em camel case, e adicionando a users.js service pra isso

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -1,5 +1,5 @@
 import { LOCAL_STORAGE_ACCESS_TOKEN_KEY, LOCAL_STORAGE_REFRESH_TOKEN_KEY } from '@/constants/local-storage';
-import { protectedApi, publicApi } from '@/lib/axios';
+import { UserService } from '@/services/user';
 import { useMutation } from '@tanstack/react-query'
 import { createContext, useContext, useEffect, useState } from 'react'
 import { toast } from 'sonner';
@@ -32,23 +32,16 @@ export const AuthContextProvider = ({ children }) => {
     const signupMutation = useMutation({
         mutationKey: ['signup'],
         mutationFn: async (variables) => {
-            const response = await publicApi.post('/users', {
-                first_name: variables.firstName,
-                last_name: variables.lastName,
-                email: variables.email,
-                password: variables.password,
-            })
-            return response.data
+            const response = await UserService.signup(variables)
+            return response
         },
     })
 
     const loginMutation = useMutation({
         mutationKey: ['login'],
         mutationFn: async (variables) => {
-            const response = await publicApi.post('/users/login', {
-                password: variables.password,
-            })
-            return response.data
+            const response = await UserService.login(variables)
+            return response
         },
     })
 
@@ -59,8 +52,8 @@ export const AuthContextProvider = ({ children }) => {
                 const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY)
                 const refreshToken = localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY)
                 if (!accessToken && !refreshToken) return
-                const response = await protectedApi.get('/users/me')
-                setUser(response.data)
+                const response = await UserService.me()
+                setUser(response)
             } catch (error) {
                 setUser(null)
                 console.error(error)
---
+++ src/services/user.js
@@ -0,0 +1,44 @@
+import { protectedApi, publicApi } from "@/lib/axios"
+
+export const UserService = {
+
+    signup: async(input) => {
+        const response = await publicApi.post('/users', {
+                first_name: input.firstName,
+                last_name: input.lastName,
+                email: input.email,
+                password: input.password,
+            })
+            return {
+            id: response.data.id,
+            email: response.data.email,
+            firstName: response.data.first_name,
+            lastName: response.data.last_name,
+            tokens: response.data.tokens
+        }
+    },
+
+    login: async (input) => {
+         const response = await publicApi.post('/users/login', {
+                email: input.email,
+                password: input.password,
+            })
+            return {
+            id: response.data.id,
+            email: response.data.email,
+            firstName: response.data.first_name,
+            lastName: response.data.last_name,
+            tokens: response.data.tokens
+        }
+    },
+
+    me: async () => {
+        const response = await protectedApi.get('/users/me')
+        return {
+            id: response.data.id,
+            email: response.data.email,
+            firstName: response.data.first_name,
+            lastName: response.data.last_name,
+        }
+    }
+}
